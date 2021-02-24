5063e6c34f2fb50cb40e02d41a8ad0ff52cf35f3
AdaLVT <79543286+AdaLVT@users.noreply.github.com>
2021-02-23 23:37:16 -0500

    Update pVerify.d

--- pVerify.d
+++ pVerify.d
@@ -1,6 +1,7 @@
 //Project Verify
 
 import std.variant;
+import std.stdio;
 
 /++
 Plans for a theorem verifier written in D, which outputs "me-readable" notation.
@@ -25,26 +26,173 @@ or void for an assertion without proof ++/
 
 enum ProofType { WFF, Assumption, Set, Class }
 
-class Expression {} //Will sketch out later
-/++ Internal representation of expressions, so something like:
-Implies(Implies(Not(x), Not(y)), Implies(y, x)) ++/
+/+ [[ WFF ]]: formulas with a true or false value.
+(( Assumption )): something that is true.
+{{ Set }}: Some parameter that is a set
+<< Class >>: Some parameter that is a class
++/
+
+interface Exp {
+	public bool isAtomic();
+	public bool isNegation();
+	public bool isImplication(); 
+}
+
+/++An interface to implement the `isX()` features of Expression.++/
+
+abstract class Expression: Exp {
+	private string representation;
+	private Expression[] arguments;
+
+	public string getRepresentation() {
+		return this.representation;
+	}
+	
+	public Expression[] getArguments() {
+		return this.arguments;
+	}
+
+}
+/+ And abstract class that the individual expression types inherit from +/
+
+class Atomic: Expression {
+	bool isAtomic() { return true; }
+	bool isNegation() { return false;}
+	bool isImplication() { return false; }
+	this(string rep) {
+		this.representation = rep;
+		this.arguments = [];
+	}
+}
+Atomic atom(string arg) {
+	return new Atomic(arg);
+}
+
+/+ The class that is stuff like "x" "phi" "\varnothing" etc. +/
+
+class Negation: Expression {
+	bool isAtomic() { return false; }
+	bool isNegation() { return true;}
+	bool isImplication() { return false; }
+	this(Expression arg) {
+		if(arg.isAtomic || arg.isNegation)
+			this.representation = "\\lnot" ~ arg.representation;
+		else
+			this.representation = "\\lnot(" ~ arg.representation ~ ")";
+		this.arguments = [arg];
+	}
+}
+Negation not(Expression arg) {
+	return new Negation(arg);
+}
+/+ Logical negation +/
+
+class Implication: Expression {
+	bool isAtomic() { return false; }
+	bool isNegation() { return false;}
+	bool isImplication() { return true; }
+	this(Expression lhs, Expression rhs) {
+		string lhsP, rhsP;
+		if(lhs.isAtomic || lhs.isNegation)
+			lhsP = lhs.representation;
+		else
+			lhsP = "(" ~ lhs.representation ~ ")";
+		if(rhs.isAtomic || rhs.isNegation)
+			rhsP = rhs.representation;
+		else
+			rhsP = "(" ~ rhs.representation ~ ")";
+		this.representation = lhsP ~ "\\Leftrightarrow " ~ rhsP;
+		this.arguments = [lhs, rhs];
+	}
+}
+Implication implies(Expression lhs, Expression rhs) {
+	return new Implication(lhs, rhs);
+}
+
+/+ Logical implication +/
+
+
+		
+
+/++ Expression is the >internal< representation of expressions, 
+with `.representation` being the human-readable form ++/
 
 class Proof {
-	private string representation;
-	/++ In the above example, this would be: [[ ~~x v wffDN[[x]] ]] 
+	/++ In the above example, this would be: wffDN 
 	the internal representation when using a proof ++/
 	
 	private ProofType proofType;
+	public bool isWFF() {
+		return this.proofType == ProofType.WFF;
+	}
+	public bool isAssumption() {
+		return this.proofType == ProofType.Assumption;
+	}
+	public bool isSet() {
+		return this.proofType == ProofType.Set;
+	}
+	public bool isClass() {
+		return this.proofType == ProofType.Class;
+	}
 	/++ In the above example, this would be ProofType.WFF ++/
-
+	
 	private Expression expression;
+	public Expression getExpression() {
+		return this.expression;
+	}
 	/++ This would be something like Not(Not(x)),
 	an internal representation of ~~x ++/
 
 	private Justification justification;
+	public bool isAxiom() {
+		return this.justification.peek!(void) !is null;
+	}
+	public Proof getProof() {
+		return *this.justification.peek!Proof;
+	}
+	
 	/++ The justification for the step, so
 	this would be `wffNot(wffNot(x))`,
 	with a type signature of Proof wffNot(Proof x)++/
+
+	this(ProofType proofType, Expression expression) {
+		this.proofType = proofType;
+		this.expression = expression;
+	}
+	/+ For statements without proof: axioms +/
+
+	this(ProofType proofType, Expression expression, Proof justification) {
+		this.proofType = proofType;
+		this.expression = expression;
+		this.justification = justification;
+	}
+
+	public string getRepresentation() {
+		string temp = this.expression.getRepresentation();
+		if(this.justification.peek!(void) !is null) {
+			assert(true);
+		} else {
+			temp ~= " \\\n\\because " ~ (*this.justification.peek!(Proof)).getRepresentation();
+		}
+		if(this.proofType == ProofType.WFF) {
+			return "\\lBrack " ~ temp ~ " \\rBrack";
+		} else if(this.proofType == proofType.Assumption) {
+			return "\\lParen " ~ temp ~ " \\rParen";
+		} else if(this.proofType == proofType.Set) {
+			return "\\lBrace " ~ temp ~ " \\rBrace";
+		} else {
+			return "\\lAngle " ~ temp ~ " \\rAngle";
+		}
+	}
+}
+
+Proof wffFalse() {
+	return new Proof(ProofType.WFF, atom("0"));
 }
 	
-void main() {}
+void main() {
+	auto x = new Atomic("x");
+	auto y = new Atomic("y");
+	auto z = new Implication(x, new Implication(y, x));
+	writeln(z.getRepresentation());
+}
